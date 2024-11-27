09aff75c3afb70de4f621e1dc21ed3c1dbc112ce
Paul Vayssier <paul.vayssier@ynov.com>
2024-11-27 09:48:08 +0100

    Initial Commit

---
+++ M.I.A/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  M.I.A
+//
+//  Created by Paul Vayssier on 27/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ M.I.A/M_I_AApp.swift
@@ -0,0 +1,17 @@
+//
+//  M_I_AApp.swift
+//  M.I.A
+//
+//  Created by Paul Vayssier on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct M_I_AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

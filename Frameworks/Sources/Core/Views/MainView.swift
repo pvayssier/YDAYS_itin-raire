068cebd4a0233413c332bdb606bb139565aae93a
Paul Vayssier <paul.vayssier@ynov.com>
2024-11-27 11:30:42 +0100

    Frameworks

---
+++ Frameworks/Sources/Core/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Frameworks
+//
+//  Created by Paul Vayssier on 27/11/2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ Frameworks/Sources/Map/Views/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Frameworks
+//
+//  Created by Paul Vayssier on 27/11/2024.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
